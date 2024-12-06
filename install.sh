0f0529f6d341eae57047a4cb32bbf78c65e83e18
DrilonCS <73757898+DrilonCS@users.noreply.github.com>
2024-12-06 20:06:38 +0100

    Update install.sh 2

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ exec > >(tee -a $LOGFILE) 2>&1
 echo "Starte Installation..."
 
 # Docker installieren
+echo "Docker installieren..."
 sudo apt-get update && sudo apt upgrade -y
 sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -20,14 +21,17 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Docker starten und aktivieren
+echo "Docker starten..."
 sudo systemctl start docker
 sudo systemctl enable docker
 
 # Docker Compose installieren
+echo "Docker Compose installieren..."
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Git und Jq installieren
+echo "Git und jq installieren..."
 sudo apt update && sudo apt install git -y
 sudo apt install jq
 
